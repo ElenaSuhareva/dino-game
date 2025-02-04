a3beb08f530ea85e65a85e2efe8ba1279f47654c
ElenaSuhareva <162101072+ElenaSuhareva@users.noreply.github.com>
2025-02-04 17:15:22 +0300

    onTouch.js

--- script.js
+++ script.js
@@ -5,20 +5,24 @@ document.addEventListener("keydown", function (event) {
   jump();
 });
 
-function jump() {
-  if (dino.classList != "jump") {
-      dino.classList.add("jump")
-  }
-  setTimeout(function () {
-    dino.classList.remove("jump");    
-  }, 300)
-}
+document.addEventListener("onTouch", function (event) {
+  jump();
+});
+
+// function jump() {
+//   if (dino.classList != "jump") {
+//       dino.classList.add("jump")
+//   }
+//   setTimeout(function () {
+//     dino.classList.remove("jump");    
+//   }, 300)
+// }
 
 let isAlive = setInterval(function () {
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
   let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-    alert("GAME OVER!")
-  }
+  // if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+  //   alert("GAME OVER!")
+  // }
 }, 10)
